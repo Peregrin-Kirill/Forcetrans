32e1547328c2ea7fa9102c3a733151aada20ac4d
Peregrin-Kirill <91873401+Peregrin-Kirill@users.noreply.github.com>
2021-11-29 17:28:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.querySelector('.outcome').addEventListener('click', ()=>{
-    let a = document.querySelector('.distance').value;
-    let b = document.querySelector('.points').value;
-    let c = document.querySelector('.pallet').value;
-    let d = (27*a)/12*c+(550*b-1100);
-    document.querySelector('.result').innerHTML = ('Себестоимость ' + d +' рублей');
-    document.querySelector('.bid').innerHTML = ('Желаемая ставка ' + (d*1.3) + ' рублей');
-    document.querySelector('.bonus').innerHTML = ('Бонусная часть ' + ((d*1.3) - d)/100*10 + ' рублей');
+    let distance = document.querySelector('.distance').value;
+    let points = document.querySelector('.points').value;
+    let pallet = document.querySelector('.pallet').value;
+    let money = document.querySelector('.money').value;
+    let formula = (27*distance)/12*pallet+(550*points-1100);
+    document.querySelector('.result').innerHTML = ('Себестоимость ' + formula +' рублей');
+    document.querySelector('.bid').innerHTML = ('Желаемая ставка ' + (formula*1.3) + ' рублей');
+    document.querySelector('.proceeds').innerHTML = ('Выручка ' + (money-formula) + ' рублей');
+    document.querySelector('.bonus').innerHTML = ('Бонусная часть ' + (money-formula)/100*10 + ' рублей');
 })
