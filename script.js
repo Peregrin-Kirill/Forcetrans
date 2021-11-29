4cf7419536595473b424c2ce720241644dfbc3d4
Peregrin-Kirill <91873401+Peregrin-Kirill@users.noreply.github.com>
2021-11-29 17:33:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// let distance = prompt(['Расстояние']);
+// let stops = prompt(['Остановки']);
+// let volume = prompt(['Колличество палет']);
+// alert((27*distance)/12*volume+(550*stops-2));
+
+
+
+document.querySelector('.outcome').addEventListener('click', ()=>{
+    let distance = document.querySelector('.distance').value;
+    let points = document.querySelector('.points').value;
+    let pallet = document.querySelector('.pallet').value;
+    let money = document.querySelector('.money').value;
+    let formula = (27*distance)/12*pallet+(550*points-1100);
+    document.querySelector('.result').innerHTML = ('Себестоимость ' + formula +' рублей');
+    document.querySelector('.bid').innerHTML = ('Желаемая ставка ' + (formula*1.3) + ' рублей');
+    document.querySelector('.proceeds').innerHTML = ('Выручка ' + (money-formula) + ' рублей');
+    document.querySelector('.bonus').innerHTML = ('Бонусная часть ' + (money-formula)/100*10 + ' рублей');
+})
