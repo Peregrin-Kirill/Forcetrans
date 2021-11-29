e0efab8f4b45e3bd6818e0b1675c75325a342419
Peregrin-Kirill <91873401+Peregrin-Kirill@users.noreply.github.com>
2021-11-29 17:18:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ document.querySelector('.outcome').addEventListener('click', ()=>{
     let a = document.querySelector('.distance').value;
     let b = document.querySelector('.points').value;
     let c = document.querySelector('.pallet').value;
-    document.querySelector('.result').innerHTML = ((27*a)/12*c+(550*b-1100)+' рублей');
+    let d = (27*a)/12*c+(550*b-1100);
+    document.querySelector('.result').innerHTML = ('Себестоимость ' + d +' рублей');
+    document.querySelector('.bid').innerHTML = ('Желаемая ставка ' + (d*1.3) + ' рублей');
+    document.querySelector('.bonus').innerHTML = ('Бонусная часть ' + ((d*1.3) - d)/100*10 + ' рублей');
 })
