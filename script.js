742849e5125683c04f098b67bb60d423de0b8eac
Peregrin-Kirill <91873401+Peregrin-Kirill@users.noreply.github.com>
2021-12-02 18:10:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// let distance = prompt(['Расстояние']);
-// let stops = prompt(['Остановки']);
-// let volume = prompt(['Колличество палет']);
-// alert((27*distance)/12*volume+(550*stops-2));
-
-
-
 document.querySelector('.outcome').addEventListener('click', ()=>{
     let distance = document.querySelector('.distance').value;
     let points = document.querySelector('.points').value;
@@ -15,4 +8,16 @@ document.querySelector('.outcome').addEventListener('click', ()=>{
     document.querySelector('.bid').innerHTML = ('Желаемая ставка ' + (formula*1.3) + ' рублей');
     document.querySelector('.proceeds').innerHTML = ('Выручка ' + (money-formula) + ' рублей');
     document.querySelector('.bonus').innerHTML = ('Бонусная часть ' + (money-formula)/100*10 + ' рублей');
+})
+
+document.querySelector('.outcome__gor').addEventListener('click', ()=>{
+    let distance__gor = document.querySelector('.distance__gor').value;
+    let points__gor = document.querySelector('.points__gor').value;
+    let pallet__gor = document.querySelector('.pallet__gor').value;
+    let money__gor = document.querySelector('.money__gor').value;
+    let formula__gor = (3600 + (27*distance__gor) + (550*points__gor));
+    document.querySelector('.result__gor').innerHTML = ('Себестоимость ' + formula__gor +' рублей');
+    document.querySelector('.bid__gor').innerHTML = ('Желаемая ставка ' + (formula__gor*1.3) + ' рублей');
+    document.querySelector('.proceeds__gor').innerHTML = ('Выручка ' + (money__gor-formula__gor) + ' рублей');
+    document.querySelector('.bonus__gor').innerHTML = ('Бонусная часть ' + (money__gor-formula__gor)/100*10 + ' рублей');
 })
