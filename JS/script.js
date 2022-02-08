033a302f05504a34ede45c53abffb8cca554f6ca
Кирилл Перегрин <91873401+Peregrin-Kirill@users.noreply.github.com>
2022-02-08 12:53:54 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,33 @@
+const flight = document.querySelector('#flight');
+const chbox = document.querySelector('#special');
+const sity = document.querySelector('#sity');
+const clear = document.querySelector('#clear');
+
+
+
+flight.addEventListener ('click', function(){
+    const distance = document.querySelector('#distance').value;
+    const points = document.querySelector('#points').value;
+    const pallet = document.querySelector('#pallet').value;
+    document.querySelector('.cost__price').innerHTML = ('Себестоимость  ' + ((distance * 27) /12 * pallet + (points * 250 - 500)) + ' рублей')
+    document.querySelector('.вid').innerHTML = ('Желаемая ставка  ' + ((distance * 35) /12 * pallet + (points * 500 - 500)) + ' рублей')
+})
+
+
+
+sity.addEventListener ('click', function(){
+    const distance = document.querySelector('#distance').value;
+    const points = document.querySelector('#points').value;
+    document.querySelector('.cost__price').innerHTML = ('Себестоимость  ' + ((distance * 27 ) + ( points * 250)) + ' рублей')
+    document.querySelector('.вid').innerHTML = ('Желаемая ставка  ' + ((distance * 45 ) + ( points * 500)) + ' рублей')
+})
+
+
+
+clear.addEventListener ('click', function(){
+    const distance = document.querySelector('#distance').value;
+    const points = document.querySelector('#points').value;
+    const pallet = document.querySelector('#pallet').value;
+    document.querySelector('.cost__price').innerHTML = ('');
+    document.querySelector('.вid').innerHTML = ('');
+})
