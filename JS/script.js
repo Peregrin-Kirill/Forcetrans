b8e929a387fadf33a86525038e6422d4ea02c47c
Кирилл Перегрин <91873401+Peregrin-Kirill@users.noreply.github.com>
2022-04-25 17:50:37 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -9,8 +9,8 @@ flight.addEventListener ('click', function(){
     const distance = document.querySelector('#distance').value;
     const points = document.querySelector('#points').value;
     const pallet = document.querySelector('#pallet').value;
-    document.querySelector('.cost__price').innerHTML = ('Себестоимость  ' + ((distance * 27) + (points * 250 - 500)) + ' рублей')
-    document.querySelector('.вid').innerHTML = ('Желаемая ставка  ' + ((distance * 35) + (points * 500 - 500)) + ' рублей')
+    document.querySelector('.cost__price').innerHTML = ('Себестоимость  ' + ((distance * 28) + (points * 250 - 500)) + ' рублей')
+    document.querySelector('.вid').innerHTML = ('Желаемая ставка  ' + ((distance * 36) + (points * 500 - 500)) + ' рублей')
 })
 
 
@@ -18,7 +18,7 @@ flight.addEventListener ('click', function(){
 sity.addEventListener ('click', function(){
     const distance = document.querySelector('#distance').value;
     const points = document.querySelector('#points').value;
-    document.querySelector('.cost__price').innerHTML = ('Себестоимость  ' + ((distance * 27 ) + ( points * 250)) + ' рублей')
+    document.querySelector('.cost__price').innerHTML = ('Себестоимость  ' + ((distance * 28 ) + ( points * 250)) + ' рублей')
     document.querySelector('.вid').innerHTML = ('Желаемая ставка  ' + ((distance * 45 ) + ( points * 500)) + ' рублей')
 })
 
