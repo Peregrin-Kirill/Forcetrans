6f91616756fd59a2b17aec9ef98d8c05efec6108
Кирилл Перегрин <91873401+Peregrin-Kirill@users.noreply.github.com>
2022-05-11 18:19:15 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -10,7 +10,11 @@ flight.addEventListener ('click', function(){
     const points = document.querySelector('#points').value;
     const pallet = document.querySelector('#pallet').value;
     document.querySelector('.cost__price').innerHTML = ('Себестоимость  ' + ((distance * 28) + (points * 250 - 500)) + ' рублей')
-    document.querySelector('.вid').innerHTML = ('Желаемая ставка  ' + ((distance * 36) + (points * 500 - 500)) + ' рублей')
+    if (chbox.checked) {
+    document.querySelector('.вid').innerHTML = ('Желаемая ставка  ' + (((distance * 35 - distance * 28)/30)*pallet).toFixed(0) + ' рублей')
+    } else {
+    document.querySelector('.вid').innerHTML = ('Желаемая ставка  ' + (distance * 28 + ((distance * 35 - distance * 28)/30)*pallet).toFixed(0) + ' рублей')
+    }
 })
 
 
